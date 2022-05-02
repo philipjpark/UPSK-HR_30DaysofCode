f32e35977c0b756eb91876049c15919ca16a7761
philipjpark <66290921+philipjpark@users.noreply.github.com>
2022-05-01 20:28:59 -0400

    Data type practice

---
+++ 30-Days-of-Code_Java/DataTypes.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class DataTypes {
+    
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+int num = 12;
+double num2 = 4.0;
+String word = "is the best place to learn and practice coding!";
+
+int sum = 0;
+double doubleSum = 0;
+String word2;
+
+sum = num + i;
+doubleSum = num2 + d;
+word2 = s + word;
+
+System.out.println(sum);
+System.out.println(doubleSum);
+System.out.println(word2);
+
+
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
+
